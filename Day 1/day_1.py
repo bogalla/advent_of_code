82ef89b65164278a095202e2f21c9ff981f5cc85
bogalla <brianne.ogallagher@gmail.com>
2020-12-01 20:33:53 -0500

    refactor day 1

--- Day 1/day_1.py
+++ Day 1/day_1.py
@@ -1,11 +1,12 @@
 # this is day 1
+def part_one():
+    for i in numbers:
+        for j in numbers:
+            if i + j == 2020:
+                return i * j
 
-def find_answer(filename):
-    file = open(filename, 'r')
-    numbers = []
-    for x in file:
-        numbers.append(int(x))
-    print(numbers)
+
+def part_two():
     for i in numbers:
         for j in numbers:
             for k in numbers:
@@ -13,6 +14,16 @@ def find_answer(filename):
                     return i*j*k
 
 
+def file_to_list(filename):
+    file = open(filename, 'r')
+    number_list = []
+    for x in file:
+        number_list.append(int(x))
+    return number_list
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(find_answer('input.txt'))
+    numbers = file_to_list('input.txt')
+    print(part_one())
+    print(part_two())
