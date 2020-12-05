4e9bbfafa8115a9e0d535906651fe1f80f47a497
bogalla <brianne.ogallagher@gmail.com>
2020-12-05 17:32:19 -0500

    optimize day 5

--- 2020/Day 5/main.py
+++ 2020/Day 5/main.py
@@ -8,51 +8,36 @@ def file_to_list(filename):
 
 def part_one():
     seat_list = file_to_list("input.txt")
-    # print(seat_list)
     final_list = []
-    row = 0
-    seat = 0
     for entry in seat_list:
-        # print(entry)
         i = 0
         lower_bound = 0
         upper_bound = 127
         while i < 7:
             midpoint = (upper_bound + lower_bound) / float(2)
-            # print("midpoint " + str(midpoint))
-            # print(entry[i])
             if entry[i] == 'F':
                 upper_bound = math.floor(midpoint)
             elif entry[i] == 'B':
                 lower_bound = math.ceil(midpoint)
             i += 1
-            # print("upper: " + str(upper_bound))
-            # print("lower: " + str(lower_bound))
         row = upper_bound
-        # print("--------------------------------")
         lower_bound = 0
         upper_bound = 7
         while i < 10:
-            # print(entry[i])
             midpoint = (upper_bound + lower_bound) / float(2)
-            # print("midpoint: " + str(midpoint))
             if entry[i] == 'L':
                 upper_bound = math.floor(midpoint)
             elif entry[i] == 'R':
                 lower_bound = math.ceil(midpoint)
             i += 1
-            # print("upper: " + str(upper_bound))
-            # print("lower: " + str(lower_bound))
         seat = upper_bound
         final_list.append((row*8)+seat)
-    # print(final_list)
-    print(sorted(final_list))
     return sorted(final_list)
 
 
-def multiply(listi):
+def multiply(inputs):
     maximum = 0
-    for item in listi:
+    for item in inputs:
         if item > maximum:
             maximum = item
     return maximum
@@ -61,13 +46,12 @@ def multiply(listi):
 def part_two():
     final_list = part_one()
     i = 0
-    while i < len(final_list):
+    while i < len(final_list)-1:
         if final_list[i+1] != final_list[i] + 1:
-            print(final_list[i])
+            return final_list[i + 1]
         i += 1
 
 
 if __name__ == '__main__':
     # print("final : " + str(multiply(part_one())))
     print("final : " + str(part_two()))
-    # print(part_three())
