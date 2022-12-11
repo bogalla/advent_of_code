5e2d5b7546a65f24eae1a7de00f09afa22eda4d7
Brianne <brianne@xguard.ca>
2022-12-11 14:15:09 -0500

    2022 day 5

--- 2022/Day 5/main.py
+++ 2022/Day 5/main.py
@@ -1,34 +1,83 @@
+import re
+
 def file_to_list(filename):
     file = open(filename, 'r')
-    return [line.strip() for line in file]
+    return [line for line in file]
 
-# rock = A, X, 1 point. paper = B, Y, 2 points. scissors = C, Z, 3 points.
 def part_one():
     inputs = file_to_list("input.txt")
     print(inputs)
+    stacks_e, i = get_stacks(inputs)
+    print(stacks_e)
+    stacks = populate_stack(inputs, stacks_e, i)
+    print(inputs[i+2:])
+    for x in inputs[i+2:]:
+        orders = re.findall(r'\b\d+\b', x)
+        print(orders)
+        for y in range(int(orders[0])):
+            temp = stacks[int(orders[1])-1].pop()
+            stacks[int(orders[2])-1].append(temp)
+            print(stacks)
 
-    return 0
+    ans = ""
+    for stack in stacks:
+        ans += stack.pop()
+    return ans
 
-# rock = A, 1 point. paper = B, 2 points. scissors = C, 3 points. X = lose. Y = draw. Z = win
+def populate_stack(inputs, stacks, i):
+    print(i)
+    cutlists = inputs[:i]
+    cutlists.reverse()
+    # print(cutlists)
+    for line in cutlists:
+        count = 0
+        for charIndex in (range(1, len(line), 4)):
+            # print(line[charIndex], charIndex)
+            if line[charIndex] != " ":
+                stacks[count].append(line[charIndex])
+            count+=1
+    print(stacks)
+    return stacks
+
+def get_stacks(inputs):
+    stackline = ""
+    index = 0
+    for i, line in enumerate(inputs):
+        if line.startswith(" 1"):
+            stackline = line
+            index = i
+            break
+    nospacestackline = stackline.replace(" ", "")
+    stacks = []
+    for inst in nospacestackline.strip():
+        stacks.append([])
+    return stacks, index
+
+
 def part_two():
     inputs = file_to_list("input.txt")
     print(inputs)
-    ans = []
-    triple = []
-    for ruck in inputs:
-        triple.append(ruck)
-        if len(triple) == 3:
-            print(triple)
-            for letter in triple[0]:
-                if letter in triple[1] and letter in triple[2]:
-                    ans.append(letter)
-                    break
-            triple = []
+    stacks_e, i = get_stacks(inputs)
+    print(stacks_e)
+    stacks = populate_stack(inputs, stacks_e, i)
+    print(inputs[i + 2:])
+    for x in inputs[i + 2:]:
+        orders = re.findall(r'\b\d+\b', x)
+        # print(orders) # move 1 from 2 to 3
+        group = []
+        for y in range(int(orders[0])): # for y in move 3
+            temp = stacks[int(orders[1]) - 1].pop() # take 3 from second num
+            group.append(temp)
+        print("group", group)
+        # print(stacks[int(orders[2]) - 1] + group)
+        group.reverse()
+        stacks[int(orders[2]) - 1] = stacks[int(orders[2]) - 1] + group
+        print((stacks))
 
-    sum = 0
-    for letter in ans:
-        sum += letter_to_priority(letter)
-    return sum
+    ans = ""
+    for stack in stacks:
+        ans += stack.pop()
+    return ans
 
 
 if __name__ == '__main__':
